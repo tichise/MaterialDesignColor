a984847c0750dacc59a95747d011af42cb3f9415
tichise <ichise@gmail.com>
2015-06-09 23:46:22 +0900

    fix sample code

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -9,21 +9,36 @@
 import UIKit
 import MaterialDesignColor
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     @IBOutlet var sampleImageView:UIImageView!
     @IBOutlet var sampleLabel:UILabel!
+    var colors:Array<UIColor>! = [MaterialDesignColor.orange50, MaterialDesignColor.orange100, MaterialDesignColor.orange200, MaterialDesignColor.orange300, MaterialDesignColor.orange400, MaterialDesignColor.orange500, MaterialDesignColor.orange600, MaterialDesignColor.orange700, MaterialDesignColor.orange800, MaterialDesignColor.orange900]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        sampleLabel.textColor = MaterialDesignColor.lightGreen400
-        sampleLabel.text = "color"
+        self.view.backgroundColor = UIColor.blackColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("SampleCell", forIndexPath: indexPath) as! UITableViewCell
+    
+        cell.backgroundColor = colors[indexPath.row]
+        
+        return cell
     }
 }
 
