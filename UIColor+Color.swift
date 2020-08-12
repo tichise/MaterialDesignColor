45ca7568ceb2418cc678b0e8d52249ca87f8d943
tichise <43707+tichise@users.noreply.github.com>
2020-08-12 09:52:56 +0900

    support for SwiftUI

---
+++ UIColor+Color.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Color.swift
+//  MaterialDesignColor
+//
+//  Created by tichise on 2020年8月12日 20/08/12.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+extension UIColor {
+    
+    public var suColor: Color {
+        Color(self)
+    }
+}
