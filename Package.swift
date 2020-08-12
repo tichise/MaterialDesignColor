6ceb1c7445d370d27bc76f76b0dfb048179b2943
tichise / Takuya Ichise <43707+tichise@users.noreply.github.com>
2020-08-12 09:23:28 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "MaterialDesignColor"
+    name: "MaterialDesignColor",
+    products: [
+        .library(name: "MaterialDesignColor", targets: ["MaterialDesignColor"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "MaterialDesignColor", path: "Sources"),
+    ],
+    swiftLanguageVersions: [.v5]
 )
