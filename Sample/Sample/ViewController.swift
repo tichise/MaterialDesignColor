9893c946ea5c6f8b4e80794317092d6c72b128a4
tichise <43707+tichise@users.noreply.github.com>
2020-08-12 09:35:21 +0900

    refactoring

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -2,8 +2,6 @@
 //  ViewController.swift
 //  Sample
 //
-//  Created by tichise on 2015年5月10日 15/05/10.
-//  Copyright (c) 2015年 tichise. All rights reserved.
 //
 
 import UIKit
@@ -13,14 +11,24 @@ class ViewController: UITableViewController {
     
     @IBOutlet var sampleImageView:UIImageView!
     @IBOutlet var sampleLabel:UILabel!
-    var colors:Array<UIColor>! = [MaterialDesignColor.orange50, MaterialDesignColor.orange100, MaterialDesignColor.orange200, MaterialDesignColor.orange300, MaterialDesignColor.orange400, MaterialDesignColor.orange500, MaterialDesignColor.orange600, MaterialDesignColor.orange700, MaterialDesignColor.orange800, MaterialDesignColor.orange900]
+    
+    var colors = [MaterialDesignColor.orange50,
+                                  MaterialDesignColor.orange100,
+                                  MaterialDesignColor.orange200,
+                                  MaterialDesignColor.orange300,
+                                  MaterialDesignColor.orange400,
+                                  MaterialDesignColor.orange500,
+                                  MaterialDesignColor.orange600,
+                                  MaterialDesignColor.orange700,
+                                  MaterialDesignColor.orange800,
+                                  MaterialDesignColor.orange900]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.black
+        self.view.backgroundColor = .black
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
